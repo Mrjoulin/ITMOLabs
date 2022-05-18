008657eb2c8709d68de451ca611029c8c480be3f
smyts <mytsstepan@gmail.com>
2022-05-18 15:42:08 +0300

    Add login and sign up windows

--- semester_2/ProgLabs/Lab08/client/src/main/kotlin/main.kt
+++ semester_2/ProgLabs/Lab08/client/src/main/kotlin/main.kt
@@ -1,6 +1,15 @@
 import client.Authorization
 import client.Client
 import client.ClientSession
+import javafx.application.Application
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.TextField
+import javafx.scene.shape.Circle
+import javafx.stage.Stage
 import utils.logger
 
 import java.lang.Exception
@@ -54,6 +63,22 @@ class Main {
 }
 
 
+class JFXMLApplication : Application() {
+    override fun start(primaryStage: Stage) {
+        val root: Parent = FXMLLoader.load(javaClass.classLoader.getResource("login.fxml"))
+        val scene = Scene(root, 500.0, 600.0)
+        scene.stylesheets.add(javaClass.classLoader.getResource("loginstyle.css")!!.toExternalForm())
+        primaryStage.scene = scene
+
+        primaryStage.show()
+    }
+
+    fun go() {
+        launch()
+    }
+
+}
+
 fun main() {
-    Main().start()
+    JFXMLApplication().go()
 }
